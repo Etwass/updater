7a16a60c59dbee78d90579ac2cc9d177a107359f
Etwass <spamreceiver6@outlook.com>
2026-05-15 16:14:41 +0200

    The cURL library has been added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
-#include <stdio.h>
+#include <curl/curl.h>
+#include <cstdio>
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream)
+  {
+    return fwrite(ptr, size, nmemb, stream);
+  }
 
 int main()
   {
-    printf("Hello, World!\n");
+    CURL *curl;
+    FILE *fp;
+    CURLcode res;
+    const char *url = "ftp://example.com/path/to/file.txt";
+    const char *outfilename = "file.txt";
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if(curl)
+      {
+        fp = fopen(outfilename, "wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        // Если требуется аутентификация:
+        // curl_easy_setopt(curl, CURLOPT_USERPWD, "username:password");
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        fclose(fp);
+        if(res != CURLE_OK)
+          {
+            fprintf(stderr, "curl_easy_perform() failed: %s", curl_easy_strerror(res));
+              return 1;
+          }
+      }
+    curl_global_cleanup();
     return 0;
   }
